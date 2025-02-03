c3d77f82841e4389fb16354e0a5fe462004ef97a
spark1k <153033063+spark1k@users.noreply.github.com>
2025-02-02 18:19:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "e": "",
+    "mathinter.mysaol.com": "",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
