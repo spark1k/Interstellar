9bb4d973aa53ed2a6b1dad8a40bc9933c024a546
spark1k <153033063+spark1k@users.noreply.github.com>
2025-02-02 18:18:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "gointerstellar.app": "",
+    "mathinter.mysaol.com": "",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstell: "interstellisgoated",
+    inter: "interisgoated",
   },
 };
 
